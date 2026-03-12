24596a92bef215661c9ecbb026d17a1c35c7d00d
GabrielDaiu7 <gabrieldaiu909@gmail.com>
2026-03-12 18:08:49 +0100

    update

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -2,32 +2,26 @@ import logo from './assets/holberton-logo.jpg';
 import { getCurrentYear, getFooterCopy } from "./utils";
 import Notifications from "./Notifications";
 
-import './App.css'
+import './App.css';
 
 const App = () => {
-  const date = new Date()
-
-
   return (
     <>
       <div className="root-notifications">
         <Notifications />
       </div>
-      <div className='App-header'>
+      <div className="App-header">
         <img src={logo} alt="holberton logo" />
-        <h1 style={{ color:'#e1003c'}}>School dashboard</h1>
+        <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
       </div>
-      <div className='App-body'>
-
-        <p >Login to access the full dashboard</p>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
       </div>
-
-      <div className='App-footer'>
-
-        <p >Copyright {getCurrentYear()} - {getFooterCopy(false)}</p>
+      <div className="App-footer">
+        <p>Copyright {getCurrentYear()} - {getFooterCopy(false)}</p>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default App;
