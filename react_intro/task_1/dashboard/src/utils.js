5348d8704e09f5c7d3746a60da75a5715ce2973e
GabrielDaiu7 <gabrieldaiu909@gmail.com>
2026-03-12 18:28:48 +0100

    update

--- react_intro/task_1/dashboard/src/utils.js
+++ react_intro/task_1/dashboard/src/utils.js
@@ -3,7 +3,8 @@ export const getCurrentYear = () => new Date().getFullYear();
 export const getFooterCopy = (isIndex) =>
   isIndex ? 'Holberton School' : 'Holberton School main dashboard';
 
-export default {
-  getCurrentYear,
-  getFooterCopy,
-};
+const utils = (isIndex) => getFooterCopy(isIndex);
+utils.getCurrentYear = getCurrentYear;
+utils.getFooterCopy = getFooterCopy;
+
+export default utils;
